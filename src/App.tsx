e91e37038619f14dfdc2254cac56b3a67db6f574
ImanM9 <imanmotlagh17@gmail.com>
2024-11-09 18:40:31 -0500

    route changes

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import ProfileHome from './component/profile/profileHome'
 import VoiceRecorder from './component/talking/talking'
+import Login from './component/home/login'
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<VoiceRecorder />} />
+        <Route path="/" element={<Login />} />
         <Route path="/profile" element={<ProfileHome />} />
+        <Route path="/home" element={<VoiceRecorder />} />
       </Routes>
     </Router>
   )
